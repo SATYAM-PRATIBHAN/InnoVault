c044766428e87430dfc7f3a4abde56b83f1dbb52
Satyam <satyampratibhan@gmail.com>
2025-02-06 13:59:58 +0530

    updated navbar

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -61,6 +61,9 @@ export default function Navbar() {
                     <Link href="/claimed" className="text-black py-2 hover:underline w-full text-center" onClick={() => setMenuOpen(false)}>
                         Claimed
                     </Link>
+                    <Link href="/completedProjects" className="text-black py-2 hover:underline w-full text-center" onClick={() => setMenuOpen(false)}>
+                        Completed Projects
+                    </Link>
                     <a href="https://github.com/SATYAM-PRATIBHAN/InnoVault">
                         <button className="mt-2 px-6 py-2 rounded-xl border font-bold border-none text-black bg-white hover:bg-gray-300 hover:text-black transition duration-300 flex items-center gap-2">
                             <FaGithub size={18} color="black" /> <span>Star on Github</span>
